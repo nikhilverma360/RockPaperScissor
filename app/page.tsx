26d31479fa5be60e097b1a1f4ee50ed6d8718aff
Nikhil Verma <nv5475609@gmail.com>
2024-02-05 15:10:29 +0530

    curser , background ,walletconnect

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,36 @@
+"use client";
 import Dashboard from "./dashboard/dashboard";
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from "wagmi";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function Home() {
-  return (<>
-  
-  <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
+  const { status } = useAccount();
+  return (
+    <div className="bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-black via-gray-800 to-black animate-text">
+      <AnimatedCursor
+        innerSize={18}
+        outerSize={25}
+        innerScale={3}
+        outerScale={2}
+        outerAlpha={0}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color)",
+        }}
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: 12,
+        }}
+      >
+        <w3m-button />
+      </div>
+      {/* <Dashboard/> */}
+      {status === "connected" ? <Dashboard /> : <w3m-button />}
     </div>
-    <Dashboard/>
-    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,6 +68,11 @@ const config = {
           to: { height: "0" },
         },
       },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
