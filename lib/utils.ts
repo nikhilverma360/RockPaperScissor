b1270a670ee3db210e53af4ed124797909ad7032
Nikhil Verma <nv5475609@gmail.com>
2024-02-10 01:20:51 +0530

    getImageUrl() and copytoClipboard() as utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,41 @@
+import { GameMove } from "@/types/types";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getImageUrl = (input?: string): string => {
+  switch (input) {
+      case "1":
+          return('/rock.png');
+          
+      case "2":
+          return('/paper.png');
+        
+      case "3":
+          return('/scissors.png');
+         
+      case "4":
+          return('/spock.png');
+
+      case "5":
+          return('/lizard.png');
+    
+      default:
+          return('');
+     
+  }
+};
+
+export function copyToClipboard(address: string) {
+    const el = document.createElement('textarea');
+    el.value = address;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+
