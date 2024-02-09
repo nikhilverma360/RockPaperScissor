1e3b606fceba58dd59a48fabc0456d05b8659b60
Nikhil Verma <nv5475609@gmail.com>
2024-02-10 01:22:27 +0530

    Update useHasher.ts

--- hooks/useHasher.ts
+++ hooks/useHasher.ts
@@ -8,10 +8,9 @@ const useHasher = () => {
   const { signMessageAsync: wagmiSignAsync } = useSignMessage();
 
   const getMessageForSigning = (existingPassword?: string) => {
-    const password = existingPassword || `${Date.now()}/${randomBytes(8).toString("hex")}`;
+    const password = existingPassword || `*${Date.now()}#${randomBytes(8).toString("hex")}`;
     const message =
       "Signature of this message proves commitment to your move without revealing it." +
-      " Signing a message will not initiate an Ethereum transaction." +
       `\n\nGame password: ${password}`;
     return { message, password };
   };
@@ -29,17 +28,7 @@ const useHasher = () => {
   const hashGameMessage = (move: GameMove, salt: `0x${string}`) =>
     keccak256(encodePacked(["uint8", "uint256"], [move, BigInt(salt)]));
 
-  const deriveMoveFromSalt = (commitment?: `0x${string}`, salt?: `0x${string}`) => {
-    if (!commitment || !salt) return GameMove.NULL;
-
-    for (let i = 1; i <= 5; i++) {
-      if (commitment === hashGameMessage(i, salt)) return i as GameMove;
-    }
-
-    return GameMove.NULL;
-  };
-
-  return { getMessageForSigning, signMessage, hashGameMessage, deriveMoveFromSalt };
+  return { getMessageForSigning, signMessage, hashGameMessage };
 };
 
 export default useHasher;
