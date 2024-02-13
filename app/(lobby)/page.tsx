05a2a139faedc731fe9a6acd7cf7f0a8a4119ce7
Nikhil Verma <nv5475609@gmail.com>
2024-02-14 05:14:03 +0530

    Update page.tsx

--- app/(lobby)/page.tsx
+++ app/(lobby)/page.tsx
@@ -12,7 +12,10 @@ export default function Home() {
       <Navbar />
       <div className="flex min-h-screen flex-col items-center justify-between">
         <div className="grid grid-cols-1 mt-8 mx-2  lg:grid-cols-3 gap-16">
-          <div>{status === "connected" ? <Dashboard /> : <w3m-button />}</div>
+          {/* <div>{status === "connected" ? <Dashboard /> : <w3m-button />}</div> */}
+          <div>
+            <Dashboard />
+          </div>
           <div className="col-span-2">
             <MyGames />
           </div>
