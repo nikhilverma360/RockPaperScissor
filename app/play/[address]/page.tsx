c8c9985d212b0bb7fc440133bab015aa9c0d2833
Nikhil Verma <nv5475609@gmail.com>
2024-02-14 05:07:36 +0530

    game play integration

--- app/play/[address]/page.tsx
+++ app/play/[address]/page.tsx
@@ -1,27 +1,18 @@
 "use client";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import Navbar from "@/components/navbar";
+import useGame from "@/hooks/useGames";
+import { P2Move } from "@/app/play/[address]/(ui)/P2Move";
+import { Wait } from "./(ui)/WaitOrTimeout";
+import { useAccount } from "wagmi";
+import { PlayerRole } from "@/types/types";
+import { Player2 } from "./(ui)/Player2";
+import { Player1 } from "./(ui)/Player1";
+import { GameOver } from "./(ui)/GameOver";
 
 const formSchema = z.object({
   move: z.enum(["1", "2", "3", "4", "5"], {
@@ -29,7 +20,14 @@ const formSchema = z.object({
   }),
 });
 
-export default function Page() {
+export default function Page({
+  params,
+}: {
+  params: { address: `0x${string}` };
+}) {
+  const { playerRole, c2, stake } = useGame(params.address);
+  const { status } = useAccount();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -37,99 +35,38 @@ export default function Page() {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    //clickSound();
-    // const signature = await signMessage(message)
-    //   .then((sign) => {
-    //     createGame(
-    //       values.OpponentAddress as `0x${string}`,
-    //       values.BetAmount,
-    //       values.move as unknown as GameMove,
-    //       keccak256(sign!)
-    //     );
-    //   })
-    //   .catch((e) => {
-    //     console.error(e);
-    //   });
-  }
+  async function onSubmit(values: z.infer<typeof formSchema>) {}
   return (
     <div>
-      <Navbar/>
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>Choose Your Move</CardTitle>
-            <CardDescription>
-              Choose your move and play against the player 1
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
-              >
-                <FormField
-                  control={form.control}
-                  name="move"
-                  render={({ field }) => (
-                    <FormItem className="space-y-3">
-                      <FormControl>
-                        <RadioGroup
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                          className="grid grid-cols-3  space-y-1"
-                        >
-                          <FormItem className="flex flex-col items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem src="/rock.png" value="1" />
-                            </FormControl>
-                            <FormLabel className="font-normal">Rock</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex flex-col items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem src="/paper.png" value="2" />
-                            </FormControl>
-                            <FormLabel className="font-normal">Paper</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex flex-col items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem src="/scissors.png" value="3" />
-                            </FormControl>
-                            <FormLabel className="font-normal">
-                              Scissors
-                            </FormLabel>
-                          </FormItem>
-                          <FormItem className="flex flex-col items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem src="/spock.png" value="4" />
-                            </FormControl>
-                            <FormLabel className="font-normal">Spock</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex flex-col items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem src="/lizard.png" value="5" />
-                            </FormControl>
-                            <FormLabel className="font-normal">
-                              Lizard
-                            </FormLabel>
-                          </FormItem>
-                        </RadioGroup>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
+      <Navbar />
+      {status == "connected" && (
+        <div className="flex min-h-screen flex-col items-center justify-between p-4">
+          <div>
+            <div>
+              <Card className="flex flex-col items-center justify-betweeen mb-2">
+                <CardHeader>
+                  <CardTitle>The Bet Amount Is {String(stake)} ETH </CardTitle>
+                </CardHeader>
+              </Card>
+              {!!c2 && !stake ? (
+                <GameOver />
+              ) : (
+                <>
+                  {playerRole == PlayerRole.PLAYER2 && (
+                    <Player2 GameAddress={params.address} />
                   )}
-                />
-                <div className="flex justify-end">
-                  <Button type="submit">Play</Button>
-                </div>
-              </form>
-            </Form>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
+                  {playerRole == PlayerRole.PLAYER1 && (
+                    <Player1 GameAddress={params.address} />
+                  )}
+                  {playerRole == PlayerRole.AUDIENCE && (
+                    <Wait GameAddress={params.address} />
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
